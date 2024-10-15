f9de5f63e94ca4265f8d5aa87d6cc486191109b1
Nicothekiller <nicolasnaran@gmail.com>
2024-10-15 12:53:22 -0500

    cambios para organizar archivos mejor, quitar comentarios, etc

--- init.lua
+++ init.lua
@@ -774,12 +774,12 @@ require("lazy").setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
         },
       },
       "saadparwaiz1/cmp_luasnip",
@@ -963,34 +963,10 @@ require("lazy").setup({
       indent = { enable = true, disable = { "ruby" } },
     },
     config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
       ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  require("kickstart.plugins.debug"),
-  require("kickstart.plugins.indent_line"),
-  require("kickstart.plugins.lint"),
-  require("kickstart.plugins.autopairs"),
-  require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -1,9 +1,6 @@
 return {
-  -- NOTE: Yes, you can install new plugins here!
   "mfussenegger/nvim-dap",
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     "rcarriga/nvim-dap-ui",
 
     -- Required dependency for nvim-dap-ui
@@ -20,7 +17,7 @@ return {
     local dap = require("dap")
     local dapui = require("dapui")
     return {
-      -- Basic debugging keymaps, feel free to change to your liking!
+      -- Basic debugging keymaps
       { "<F5>", dap.continue, desc = "Debug: Start/Continue" },
       { "<F1>", dap.step_into, desc = "Debug: Step Into" },
       { "<F2>", dap.step_over, desc = "Debug: Step Over" },
@@ -33,7 +30,7 @@ return {
         end,
         desc = "Debug: Set Breakpoint",
       },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      -- Toggle to see last session result.
       { "<F7>", dapui.toggle, desc = "Debug: See last session result." },
       unpack(keys),
     }
@@ -51,10 +48,7 @@ return {
       -- see mason-nvim-dap README for more information
       handlers = {},
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         "delve",
       },
     })
