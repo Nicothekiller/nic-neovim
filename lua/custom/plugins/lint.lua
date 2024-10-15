708d4c5cb12f6becae41393fd7190482cfbf9a83
Nicothekiller <nicolasnaran@gmail.com>
2024-10-15 16:13:57 -0500

    cambios a linter

--- lua/custom/plugins/lint.lua
+++ lua/custom/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        markdown = { "markdownlint" },
+        -- markdown = { "markdownlint" },
       }
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
