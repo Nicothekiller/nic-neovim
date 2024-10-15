3dc595c78cb06854bdf1395c702850c5ad5ee3a0
Nicothekiller <nicolasnaran@gmail.com>
2024-10-15 12:56:37 -0500

    comentarios

--- init.lua
+++ init.lua
@@ -968,11 +968,7 @@ require("lazy").setup({
     end,
   },
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  -- add plugins from forlder for better organization
   { import = "custom.plugins" },
 }, {
   ui = {
