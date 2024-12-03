7c716e6fde9d3ae3558b02767ee51bc12ba77146
Nicothekiller <nicolasnaran@gmail.com>
2024-12-03 18:42:03 -0500

    added black as python formatter

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -31,7 +31,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
