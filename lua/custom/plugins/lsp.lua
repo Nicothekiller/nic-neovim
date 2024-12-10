1f78129198f9311ca7f839a1728ddee29e06cbf0
Nicothekiller <nicolasnaran@gmail.com>
2024-12-10 18:46:41 -0500

    changed when the lsp loads for better startup times

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
