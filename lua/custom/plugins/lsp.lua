3e0ced24ce6aa2d0169b55b108fb4a7c70337803
Nicothekiller <nicolasnaran@gmail.com>
2024-12-12 14:52:33 -0500

    Removed java extension, left it as comments
    Removed java extension due to being unnecesary. The lsp works, remove
    the coments to install the extension again for DAP, some refactoring,
    and other things if needed.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -255,4 +255,13 @@ return {
       },
     },
   },
+  { -- java configs
+    -- "nvim-java/nvim-java",
+    -- event = { "BufReadPre", "BufNewFile" },
+    -- config = function()
+    --   -- Java plugin setup
+    --   require("java").setup()
+    --   require("lspconfig").jdtls.setup({})
+    -- end,
+  },
 }
