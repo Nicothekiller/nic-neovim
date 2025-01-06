5feb38d9431c9c0c53d9e25650e1a9e8de39ef71
Nicothekiller <nicolasnaran@gmail.com>
2025-01-06 12:56:05 -0500

    added rust fmt for formating

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -34,6 +34,7 @@ return {
         c = { "clang-format" },
         -- Conform can also run multiple formatters sequentially
         python = { "black" },
+        rust = { "rustfmt" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
