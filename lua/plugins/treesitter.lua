d5a58b8d5e7ba0f9ddf94270cd6f32267c8cd83c
Nicothekiller <nicolasnaran@gmail.com>
2025-09-29 08:07:36 -0500

    update call to treesitter
    to avoid errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -41,7 +41,7 @@ return {
       vim.api.nvim_create_autocmd("BufReadPost", {
         pattern = "*",
         callback = function()
-          vim.treesitter.start()
+          pcall(vim.treesitter.start)
         end,
       })
     end,
