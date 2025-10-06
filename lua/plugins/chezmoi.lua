235c5e97285e4e7ef547fbbd7e396859a9f195a7
Nicothekiller <nicolasnaran@gmail.com>
2025-10-05 22:14:56 -0500

    add chezmoi plugin

---
+++ lua/plugins/chezmoi.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "xvzc/chezmoi.nvim",
+    cmd = { "ChezmoiEdit" },
+    opts = {
+      edit = {
+        watch = false,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = false,
+      },
+      telescope = {
+        select = { "<CR>" },
+      },
+    },
+    init = function()
+      -- run chezmoi edit on file enter
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function()
+          vim.schedule(require("chezmoi.commands.__edit").watch)
+        end,
+      })
+    end,
+  },
+}
