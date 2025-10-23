d21a3c588e8bd1a36b47fd4d598018bf13a9a08d
Nicothekiller <nicolasnaran@gmail.com>
2025-10-22 21:51:53 -0500

    update rust config

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -4,5 +4,34 @@ return {
     version = "^6",
     ft = "rust",
     -- lazy = false, -- This plugin is already lazy
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+      },
+    },
+
+    config = function(_, opts)
+      local codelldb = vim.fn.exepath("codelldb")
+      local codelldb_lib_ext = io.popen("uname"):read("*l") == "Linux" and ".so" or ".dylib"
+      local library_path = vim.fn.expand("$MASON/opt/lldb/lib/liblldb" .. codelldb_lib_ext)
+      opts.dap = {
+        adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
+      }
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+      if vim.fn.executable("rust-analyzer") == 0 then
+        vim.notify(
+          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+          4,
+          { title = "rustaceanvim" }
+        )
+      end
+    end,
   },
 }
