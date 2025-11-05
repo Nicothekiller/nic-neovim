f66776279ee05954c4eb19add7189b19d77b4845
Nic <nicolasnaran@gmail.com>
2025-11-05 07:36:55 -0500

    add venv selector

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,11 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  ft = "python", -- Load when opening Python files
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {}, -- if you add plugin options, they go here.
+  },
+}
