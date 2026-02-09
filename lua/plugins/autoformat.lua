bb0768dbf1fc55d35493b968bc0bbd1dafd09a24
Nicothekiller <nicolasnaran@gmail.com>
2026-02-09 17:59:05 -0500

    add typst formatter

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -36,6 +36,7 @@ return {
         -- Conform can also run multiple formatters sequentially
         python = { "black" },
         rust = { "rustfmt" },
+        typst = { "typstyle" }
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
